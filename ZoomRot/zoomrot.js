98c9d607404e950e21fd24b1e5aa37026c6f8fbb
ItsAsShrimpleAsThat <155571246+ItsAsShrimpleAsThat@users.noreply.github.com>
2025-01-14 22:00:26 -0800

    added popup in browser toolbar

--- ZoomRot/zoomrot.js
+++ ZoomRot/zoomrot.js
@@ -1,1 +1,8 @@
-console.log("Hello zoom!")
+console.log("testmogsnackduke")
+addEventListener("DOMContentLoaded", (event) => {
+    let videoplayer = document.getElementsByClassName("transcript-wrapper")[0];
+
+    videoplayer.style.width = "10px";
+
+    console.log("testttt")
+});
