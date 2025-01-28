14f80aebf886153d3186b7d57de4b8570f91057d
ItsAsShrimpleAsThat <155571246+ItsAsShrimpleAsThat@users.noreply.github.com>
2025-01-28 11:36:23 -0800

    alignment function yippeeeee

--- processor/processor.py
+++ processor/processor.py
@@ -4,6 +4,8 @@ from flask_cors import CORS, cross_origin
 import subprocess
 import montreal_forced_aligner
 from time import sleep
+import shutil
+import glob
 
 import montreal_forced_aligner.config
 import vttToTextGrid
@@ -22,12 +24,12 @@ DICTIONARY_PATH = dictionarypathfile.read()
 dictionarypathfile.close()
 
 FFMPEG_DEFAULT_CONVERT = "ffmpeg -i {inputPath} -y -vn {outputPath}"
+CORPUS_FILE_NAMES = "brainrot" # What should the converted .wav or .TextGrid files be called?
 
 CORPUS_PATH = os.path.abspath("download").replace("\\", "/") if inProcessorPath else os.path.abspath("processor/download").replace("\\", "/")
 ALIGNER_OUTPUT_PATH = CORPUS_PATH + "/output"
 MFA_ALIGN_COMMAND = "mfa align {corpusPath} {dictionaryPath} {modelPath} {outputPath} --clean --final_clean --cleanup_textgrids --uses_speaker_adaptation false"
 #if erros use --beam 100
-
 # aligner = montreal_forced_aligner.alignment.PretrainedAligner(acoustic_model_path=ACOUSTIC_MODEL_PATH, dictionary_path=DICTIONARY_PATH)
 
 app = Flask(__name__)
@@ -38,7 +40,9 @@ r"""
 @param outputPath: Path to output file. Must be .wav
 """
 def ffmpegConvert(inputPath, outputPath):
-    subprocess.call(FFMPEG_DEFAULT_CONVERT.format(inputPath=inputPath, outputPath=outputPath), shell=True)
+    if(os.path.exists(inputPath) and os.path.isfile(inputPath)):
+        subprocess.call(FFMPEG_DEFAULT_CONVERT.format(inputPath=inputPath, outputPath=outputPath), shell=True)
+        os.remove(inputPath)
 
 def align():
     export_path = os.path.join(CORPUS_PATH, "aligned")
@@ -47,14 +51,10 @@ def align():
                                              dictionaryPath=DICTIONARY_PATH,
                                              modelPath=ACOUSTIC_MODEL_PATH,
                                              outputPath=ALIGNER_OUTPUT_PATH
-                                             )
-                                             )
+                                             ))
                                                                   
 # ffmpegConvert(CORPUS_PATH + "/zoom.mp4", CORPUS_PATH + "/zoom.wav")
 # vttToTextGrid.convert(CORPUS_PATH + "/zoom.vtt", CORPUS_PATH + "/zoom.TextGrid")
-
-print("hello")
-align()
 
 @app.route("/alive")
 def alive():
@@ -66,7 +66,44 @@ def contact():
 
 @app.route("/brainrot")
 def brainrot():
+    mp4Files = glob.glob(f"{CORPUS_PATH}/*.mp4")
+    if len(mp4Files) == 0:
+        return jsonify({"success" : False})
+    
+    print(mp4Files)
+    
     return jsonify({""})
+
+def testRot():
+    mp4Files = glob.glob(f"{CORPUS_PATH}/*.mp4") # Get all mp4, wav, and vtt files in download folder
+    wavFiles = glob.glob(f"{CORPUS_PATH}/{CORPUS_FILE_NAMES}.wav")
+    vttFiles = glob.glob(f"{CORPUS_PATH}/*.vtt")
+    
+    if len(mp4Files) < 1: # Only proceed if theres 1 mp4 file or wav file
+        if len(wavFiles) == 1: # We can use wav files from previous run
+            print("wav file found, not converting mp4")
+        else:
+            return False
+    elif len(mp4Files) > 1:
+        return False
+    
+    if len(vttFiles) < 1:
+        return False
+    elif len(vttFiles) > 1:
+        return False
+
+    if len(mp4Files) == 1: # Convert mp4 to wav if we don't already have a wav file
+        mp4File = mp4Files[0]
+        ffmpegConvert(mp4File, f"{CORPUS_PATH}/{CORPUS_FILE_NAMES}.wav")
+        
+    vttFile = vttFiles[0]
+    vttToTextGrid.convert(vttFile, f"{CORPUS_PATH}/{CORPUS_FILE_NAMES}.TextGrid")
+
+    align()
+
+    return jsonify({""})
+    
+testRot()
 
 if __name__ == "__main__":
     app.run(host="localhost", port=6814)
