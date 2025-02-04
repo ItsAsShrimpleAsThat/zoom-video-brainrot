9988de0ac6c6403800287c06bc5299b4619524e2
ItsAsShrimpleAsThat <155571246+ItsAsShrimpleAsThat@users.noreply.github.com>
2025-02-04 12:07:30 -0800

    Make video vertical

--- ZoomRot/zoomrot.js
+++ ZoomRot/zoomrot.js
@@ -1,8 +1,4 @@
 addEventListener("DOMContentLoaded", (event) => {
-    let videoplayer = document.getElementsByClassName("transcript-wrapper")[0];
-
-    videoplayer.style.width = "10px";
-
     console.log("testttt")
 });
 
@@ -11,6 +7,16 @@ browser.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.message === "start")
         {
+            let videoplayer = document.getElementById("vjs_video_3_html5_api").parentElement.parentElement;
+
+            videoHeight = videoplayer.style.height.replaceAll("px", "");
+            videoWidth = videoplayer.style.width.replaceAll("px", "");
+            reversedAspectRatio = videoHeight / videoWidth; 
+            console.log(Math.floor(videoHeight * reversedAspectRatio))
+            videoplayer.style.width = Math.floor(videoHeight * reversedAspectRatio) + "px";
+
+            videoplayer.appendChild("")
+
             console.log("Starting...")
 
             fetch("http://127.0.0.1:6814/alive")
