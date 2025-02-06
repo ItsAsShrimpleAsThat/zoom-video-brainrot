da8202e9cc6964ffff72a2612f531b29532abd58
ItsAsShrimpleAsThat <155571246+ItsAsShrimpleAsThat@users.noreply.github.com>
2025-02-06 08:21:03 -0800

    Pause brainrot when zoom video gets paused

--- ZoomRot/zoomrot.js
+++ ZoomRot/zoomrot.js
@@ -1,7 +1,3 @@
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; // Check https://jquery.com/ for the current version
-document.getElementsByTagName('head')[0].appendChild(script);
-
 addEventListener("DOMContentLoaded", (event) => {
     console.log("testttt")
 });
@@ -10,34 +6,37 @@ browser.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.message === "start")
         {
-            let videoplayer = document.getElementById("vjs_video_3_html5_api").parentElement.parentElement;
+            document.getElementById("vjs_video_3_html5_api").disablePictureInPicture = "true" // Disable picture in picture
 
+            let videoplayer = document.getElementById("vjs_video_3_html5_api").parentElement.parentElement; // Get video player div so we can change its size
+            
             videoHeight = videoplayer.style.height.replaceAll("px", "");
             videoWidth = videoplayer.style.width.replaceAll("px", "");
-            reversedAspectRatio = videoHeight / videoWidth; 
+            reversedAspectRatio = videoHeight / videoWidth; // Get reversed aspect ratio so we can turn the video vertical
             console.log(Math.floor(videoHeight * reversedAspectRatio))
 
             brainrotWidth = Math.floor(videoHeight * reversedAspectRatio);
             videoplayer.style.width = brainrotWidth + "px";
 
-            let videoPlayerOverlayElement = videoplayer.getElementsByClassName("vjs-rec-overlay")[0]
+            let videoPlayerOverlayElement = videoplayer.getElementsByClassName("vjs-rec-overlay")[0] // Overlay to put youtube video in
             let brainrotPlayer = document.createElement("iframe");
 
             brainrotPlayer.id = "brainrotVideo";
             brainrotPlayer.classList.add("brainrotVideoPlayer")
             brainrotPlayer.width = brainrotWidth;
             brainrotPlayer.height = videoHeight;
-            brainrotPlayer.src = "https://www.youtube.com/embed/s600FYgI5-s?si=3EjPEFVTB8281viQ&amp;showinfo=0&amp;autohide=1&amp;mute=1&amp;controls=0?rel=0&amp;autoplay=1&amp;enablejsapi=1";
+            brainrotPlayer.src = "https://www.youtube.com/embed/s600FYgI5-s?si=3EjPEFVTB8281viQ&amp;showinfo=0&amp;autohide=1&amp;mute=1&amp;loop=1&amp;controls=0&amp;rel=0&amp;autoplay=1&amp;enablejsapi=1";
             brainrotPlayer.title = "Brainrot Video";
             brainrotPlayer.allow = "autoplay;"
             brainrotPlayer.referrerPolicy = "strict-origin-when-cross-origin"
             brainrotPlayer.style = "position: relative; pointer-events: none;";
             brainrotPlayer.style.borderRadius = "7pt"
 
-            videoPlayerOverlayElement.appendChild(brainrotPlayer);
+            videoPlayerOverlayElement.appendChild(brainrotPlayer); // Add youtube video
 
-            unselectable
-            videoPlayerOverlayElement.appendChild()
+            let pauseElement = document.getElementById("vjs_video_3"); // Used to detect when the zoom video is paused
+            brainrotPlayer.addEventListener("onReady", function() { console.log("sigma"); pauseUnpauseBrainrot(brainrotPlayer, pauseElement); })
+            videoplayer.addEventListener("click", function() { pauseUnpauseBrainrot(brainrotPlayer, pauseElement); }); // Listener to automatically pause brainrot when the zoom video is paused
 
             console.log("Starting...")
 
@@ -51,3 +50,18 @@ browser.runtime.onMessage.addListener(
         }
     }
 );
+
+function pauseUnpauseBrainrot(brainrotPlayer, pauseElement)
+{
+    console.log(pauseElement.classList)
+    if(pauseElement.classList.contains("vjs-playing"))
+    {
+        console.log("Video Paused");
+        brainrotPlayer.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*')
+    }
+    else if(pauseElement.classList.contains("vjs-paused"))
+    {
+        console.log("Video Unpaused");
+        brainrotPlayer.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*')
+    }
+}
